e291433854371d8be755d0867cc9f01afa24e6e5
arachi007 <rr1047269@swccd.edu>
2017-09-18 07:07:03 +0700

    Update unblock.js

--- unblock.js
+++ unblock.js
@@ -10,13 +10,14 @@ var drid = did;
 var gapis = 'https://www.googleapis.com/drive/v3/files/'+ drid +'?alt=media&key=AIzaSyDjQy5oGfCBWx-KxyyKQnMlH9y-oIwUhB0';
 var second = $("#second").attr("data");
 var subindo = $("#subjudul").attr("data");
-	var title = $(".single-post-title").text();
+	var title = $(".post-title").text();
+	var judul = "[mv21.me] " + title + " Subtitle Indonesia";
   var playerInstance = jwplayer("myvideo")
                 playerInstance.setup({
-   sources: [{file:gapis,"type":"mkv/mp4","label":"Sever 1", "default": "true"},{file:second,"type":"mkv/mp4","label":"Sever 2", "default": "false"},{file:"","type":"mkv/mp4","label":"Server 3", "default": "false"}],
+   sources: [{file:gapis,"type":"mkv/mp4","label":"Sever 1", "default": "true", "title":judul},{file:second,"type":"mkv/mp4","label":"Sever 2", "default": "false"},{file:"","type":"mkv/mp4","label":"Server 3", "default": "false"}],
 
    skin: "seven",
-title:title,
+title:judul,
    preload: "false",
    primary: "html5",
    autostart: "false",
@@ -40,46 +41,3 @@ tracks: [{
    });
 $("#bigframe").remove();
   });
-
-function jwreloader() {
-var did = $("#bigframe").attr("data");
-var gambar = $(".videoIframe").attr("src");
-  if (did.match("http")){
-var drid = did.replace("https://drive.google.com/file/d/","").replace("/preview","");
-  }
-  else {
-var drid = did;
-}
-var gapis = 'https://www.googleapis.com/drive/v3/files/'+ drid +'?alt=media&key=AIzaSyDjQy5oGfCBWx-KxyyKQnMlH9y-oIwUhB0';
-var second = $("#second").attr("data");
-var subindo = $("#subjudul").attr("data");
-	var title = $(".single-post-title").text();
-  var playerInstance = jwplayer("myvideo")
-                playerInstance.setup({
-   sources: [{file:gapis,"type":"mkv/mp4","label":"Sever 1", "default": "true"},{file:second,"type":"mkv/mp4","label":"Sever 2", "default": "false"},{file:"","type":"mkv/mp4","label":"Server 3", "default": "false"}],
-
-   skin: "seven",
-title:title,
-   preload: "false",
-   primary: "html5",
-   autostart: "false",
-   image: gambar,
-   width: "100%",
-   aspectratio: "16:9",
-tracks: [{
-			file: subindo,
-			label: "Subs",
-			kind:  "captions",
-			default: "true",
-			}],
-   captions: {
-   color: "#FFFF00",
-   fontSize: 15,
-   edgeStyle: "uniform",
-   backgroundOpacity: 0,
-   },
-   aboutlink:"http://www.mv21.me",
-   abouttext:"Player By Movie Video 21",
-   });
-$("#bigframe").remove();
-  }
