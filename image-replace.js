60d66b4e163265850e3efa7f76ed975068312342
arachi007 <rr1047269@swccd.edu>
2017-11-28 10:07:42 +0700

    Create image-replace.js

---
+++ image-replace.js
@@ -0,0 +1,9 @@
+$('.separator').find('img').replaceWith(function () {
+$('a').remove();
+var gambar = this.src;
+var gbrhasil = gambar.replace(/s320/g, "s500");
+   return '<amp-img src="'+gbrhasil+'" width="500" height="500" layout="responsive" alt="'+this.alt+'"></amp-img>'
+});
+$('.separator').replaceWith(function () {
+$('a').remove();
+});
